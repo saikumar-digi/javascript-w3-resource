f60e690238e94674ae4ea754c565396e3fb38bd3
saikumar <saikumar@digisprint.com>
2024-01-21 18:12:54 +0530

    added typeOf

---
+++ Basic/nullUndefinedNaN/nullUndefined.js
@@ -0,0 +1,18 @@
+let a ;
+console.log(a);
+
+let arr = [1,2,,3]
+console.log(arr[2]);
+
+function add(a,b){
+    let c = a+b
+    //return c
+}
+  let sum  = add(2,3)
+  console.log(sum);
+
+
+
+  console.log(z);
+
+  //z is not defined 
---
+++ Basic/typeOf/typeOf.js
@@ -0,0 +1,30 @@
+let a = 2
+
+console.log(typeof a);
+
+
+let b = 'sai'
+
+console.log(typeof b);
+
+let c = true;
+
+console.log(typeof c);
+
+
+let d = {
+    name:'sai'
+}
+
+console.log( typeof d );
+
+let e = [1,2,3,]
+
+console.log(typeof e +" Array");
+
+let checkofArray = Array.isArray(e)
+
+console.log(checkofArray);
+
+let j= null
+console.log(typeof j);
